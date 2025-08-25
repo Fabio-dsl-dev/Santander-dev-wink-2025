9dc31049df41ff82fd70b097ae9c76ff9ce09f34
Fabio-dsl-dev <fabio.dsl.dev@gmail.com>
2025-08-25 15:13:35 -0300

    Initial commit

---
+++ src/main/java/me/dio/Santander_dev_wink_2025/Application.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_dev_wink_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWink2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Santander_dev_wink_2025/SantanderDevWink2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Santander_dev_wink_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
